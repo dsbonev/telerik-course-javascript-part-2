a762140bc9a8d96db6c72e48a45d74f45a5d4739
Dimitar Bonev <dsbonev@gmail.com>
2013-04-15 22:31:57 +0300

    add form to parameterize the number of generated elements

--- DOM-manipulation/01.CreateDivs/js/main.js
+++ DOM-manipulation/01.CreateDivs/js/main.js
@@ -44,14 +44,46 @@
       ].join(', ') + ')';
   }
 
-  var listTempContainer = document.createDocumentFragment(),
-    divCount = 5;
-
-  for (var i = 0; i < divCount; i += 1) {
-    listTempContainer.appendChild(createDiv());
+  function addEventListener(selector, eventName, listener) {
+    document.querySelector(selector).addEventListener(eventName, listener, false);
   }
 
-  document.body.appendChild(listTempContainer);
+  var timer;
+
+  addEventListener('form', 'submit', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var hasPendingJob = !!timer;
+
+    if (hasPendingJob) {
+      return;
+    }
+
+    var listTempContainer = document.createDocumentFragment(),
+      divCount = event.target.querySelector('input').valueAsNumber || 5,
+      resultEl = event.target.querySelector('output');
+
+    resultEl.innerHTML = '';
+
+    var currentCount = 0;
+
+    timer = setInterval(function () {
+      for (var i = 0; currentCount < divCount && i < 100; i += 1, currentCount += 1) {
+        listTempContainer.appendChild(createDiv());
+      }
+
+      resultEl.appendChild(listTempContainer);
+
+      var jobDone = currentCount === divCount;
+
+      if (jobDone) {
+        clearInterval(timer);
+        timer = null;
+      }
+    }, 100);
+
+  });
 
   //https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math/random
   function getRandomInt(min, max) {
